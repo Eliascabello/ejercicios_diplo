14089a35519f8406dd71cf2b412c7871bd6276bf
Eliascabello <eliascab381@gmail.com>
2025-04-04 20:03:17 -0300

    Add files via upload

---
+++ 4abril.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Ingresá un número: "))
+numero2 = int(input("Ingresá otro número: "))
+
+
+if numero1 > numero2:
+    print(f"El número mayor es: {numero1}")
+elif numero2 > numero1:
+    print(f"El número mayor es: {numero2}")
+else:
+    print("Ambos números son iguales")
+
