857fa10a7ea354b510dd6dc3b54ced5f7258bf2f
Eliascabello <eliascab381@gmail.com>
2025-04-05 12:58:59 -0300

    5abril.py

--- 5abril.py
+++ 5abril.py
@@ -22,5 +22,44 @@ for numero in range(21):
     if numero % 2 == 0:
         print(numero, end=" ")
 
+#while condicion
+contador=0
+
+while contador < 5:
+    print(contador)
+    contador +=1
+    
+#while True:
+    print(contador)
+    contador+=1
+    if contador==5:
+        break# 
+
+contador = 1
+
+while contador <= 10:
+    print(contador)
+    contador += 1
+    
+contador = 1
+
+#break para que corte
+while True:
+    print(contador)
+    contador += 1
+    if contador > 10:
+        break
+
+#ejercicio de contraseña con while 
+contraseña= "milanesa"
+
+entrada= ""
+
+while entrada != contraseña:
+     entrada = input("Ingrese la contraseña: ")
+     if entrada != contraseña:
+        print("Contraseña incorrecta")
+    
+print("contraseña correcta")
 
 
