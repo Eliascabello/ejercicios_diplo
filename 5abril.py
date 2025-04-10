d663eb33ef4f21266c9a46e3ea95b991e725ed7a
Eliascabello <eliascab381@gmail.com>
2025-04-10 20:45:20 -0300

    Update 5abril.py

--- 5abril.py
+++ 5abril.py
@@ -22,6 +22,8 @@ for numero in range(21):
     if numero % 2 == 0:
         print(numero, end=" ")
 
+
+
 #while condicion
 contador=0
 
@@ -29,7 +31,8 @@ while contador < 5:
     print(contador)
     contador +=1
     
-#while True:
+
+    #while True:
     print(contador)
     contador+=1
     if contador==5:
@@ -50,9 +53,10 @@ while True:
     if contador > 10:
         break
 
+
+
 #ejercicio de contraseña con while 
 contraseña= "milanesa"
-
 entrada= ""
 
 while entrada != contraseña:
@@ -61,6 +65,22 @@ while entrada != contraseña:
         print("Contraseña incorrecta")
     
 print("contraseña correcta")
+
+
+#contraseña con intentos 
+contraseña= "milanesa"
+entrada= ""
+intentos= 0
+
+while entrada != contraseña and intentos < 3:
+     entrada = input("Ingrese la contraseña: ")
+     if entrada != contraseña:
+        print("Contraseña incorrecta")
+         intentos +=1
+    
+print("contraseña correcta")
+
+
 
 #adivinar el numero
 import random
@@ -75,3 +95,22 @@ while not adivino:
         adivino = True
     else:
         print("No adivinaste. Intenta de nuevo.")
+
+#adivinar con intentos 
+import random
+secreto = random.randint(1, 10)
+adivino = False
+intentos= 0
+
+while not adivino and intentos < 3:
+    intento = int(input("Adivina el número (entre 1 y 10): "))
+    intentos +=1
+
+    if intento == secreto:
+        print("adivinaste")
+        adivino = True
+    else:
+        print("No adivinaste. Intenta de nuevo.")
+
+if not adivino:
+    print(f"Se te acabaron los intentos. El número era {secreto}.")
